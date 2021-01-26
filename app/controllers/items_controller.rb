b0b8b04baab54c790a8728210ea78c361fa6a5de
yurayura420 <tatu19970625@gmail.com>
2021-01-26 16:04:14 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,4 @@
 class ItemsController < ApplicationController
-
-def index
-  
-end
-
+  def index
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w+\z/ 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w+\z/
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
   NAME_READ_REGEX = /\A[ァ-ヶー－]+\z/
   validates :nickname, presence: true
@@ -13,6 +13,4 @@ class User < ApplicationRecord
   validates_format_of :first_name_read, presence: true, with: NAME_READ_REGEX
   validates_format_of :last_name_read, presence: true, with: NAME_READ_REGEX
   validates :birth_date, presence: true
-
-         
 end
