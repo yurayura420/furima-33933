d8912e91907271c9eb45b1de85825a543fabc2ca
yurayura420 <tatu19970625@gmail.com>
2021-01-26 18:54:28 +0900

    activehashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :state
+  belongs_to :shipping_fee
+  belongs_to :area
+  belongs_to :shipping_date
+  belongs_to :category
+
+
 end
