4ff1f51ba5b02bf7a668d51ac08215788db12580
yurayura420 <tatu19970625@gmail.com>
2021-01-27 17:35:05 +0900

    editアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_items, only: [:edit, :show]
+  before_action :move_to_index, only: [:edit]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -16,7 +17,9 @@ class ItemsController < ApplicationController
     end 
   end   
   def show
-
+ 
+  end
+  def edit
     
   end
 
@@ -27,4 +30,9 @@ class ItemsController < ApplicationController
     def set_items
       @item = Item.find(params[:id])
     end  
+    def move_to_index
+      unless user_signed_in? && @item.user.id == current_user.id
+        redirect_to action: :index
+      end
+    end
 end
