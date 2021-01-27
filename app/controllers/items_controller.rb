1051b2a0e2906f4ae61d704384808abb6162aae5
yurayura420 <tatu19970625@gmail.com>
2021-01-27 18:35:28 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit,:create,:update]
   before_action :set_items, only: [:edit, :show,:update]
   before_action :move_to_index, only: [:edit]
   def index
